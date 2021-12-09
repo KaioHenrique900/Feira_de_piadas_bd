1e4c91f9f08b155a091a9b9867ca4d9068da493c
KaioHenrique900 <kaioneziohenrique@gmail.com>
2021-12-09 12:02:37 -0300

    Adicionando get_data.php

---
+++ get_data.php
@@ -0,0 +1,51 @@
+<?php
+
+session_start();
+// array for JSON response
+$response = array();
+
+// connecting to db
+$con_string = "host=ec2-35-168-80-116.compute-1.amazonaws.com port=5432 dbname=d3cnre2oc9uli5 user=blodrftcfvyshh password=0516abc94ad85d3b4e126ff67eae2e73022401049d2862f853034cd2e5e37c61";
+$con = pg_connect($con_string);
+
+$email = NULL;
+$password = NULL;
+
+$isAuth = false;
+
+// Método para mod_php (Apache)
+if(isset( $_SERVER['PHP_AUTH_USER'])) {
+    $email = $_SERVER['PHP_AUTH_USER'];
+    $password = $_SERVER['PHP_AUTH_PW'];
+} // Método para demais servers
+elseif(isset( $_SERVER['HTTP_AUTHORIZATION'])) {
+    if(preg_match( '/^basic/i', $_SERVER['HTTP_AUTHORIZATION']))
+		list($email, $password) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+}
+
+// Se a autenticação não foi enviada
+if(!is_null($username)){
+    $query = pg_query($con, "SELECT senha FROM usuario WHERE email='$email'");
+
+	if(pg_num_rows($query) > 0){
+		$row = pg_fetch_array($query);
+		if($password == $row['password']){
+			$isAuth = true;
+		}
+	}
+}
+ 
+if($isAuth) {
+	$response["success"] = 1;
+	
+	// codigo sql da sua consulta
+	$response["data"] = $_SESSION['username'];
+}
+else {
+	$response["success"] = 0;
+	$response["error"] = "falha de autenticação";
+}
+
+pg_close($con);
+echo json_encode($response);
+?>
