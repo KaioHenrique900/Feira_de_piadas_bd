1882309643c33605d0c0f957dcedc97e8dbfad0f
KaioHenrique900 <kaioneziohenrique@gmail.com>
2021-12-14 11:40:34 -0300

    Atualizando get_data.php

--- get_data.php
+++ get_data.php
@@ -8,19 +8,15 @@ $response = array();
 $con_string = "host=ec2-35-168-80-116.compute-1.amazonaws.com port=5432 dbname=d3cnre2oc9uli5 user=blodrftcfvyshh password=0516abc94ad85d3b4e126ff67eae2e73022401049d2862f853034cd2e5e37c61";
 $con = pg_connect($con_string);
 
-$username = NULL;
-$password = NULL;
-
 $isAuth = false;
 
-if(isset( $_POST['email'])) {
+if(isset($_POST['email'])) {
     $username = $_POST['email'];
-}
-
-if(!is_null($username)){
-    $query = pg_query($con, "SELECT email FROM usuario WHERE email='$username'");
+    $query = pg_query($con, "SELECT email, nome FROM usuario WHERE email='$username'");
 	if(pg_num_rows($query) > 0){
+		$row = pg_fetch_array($query);
 		$isAuth = true;
+		$nome=$row['nome'];
 	}
 }
  
@@ -28,7 +24,7 @@ if($isAuth) {
 	$response["success"] = 1;
 	
 	// codigo sql da sua consulta
-	$response["data"] = $username;
+	$response["data"] = $nome;
 }
 else {
 	$response["success"] = 0;
