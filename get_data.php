4bff74a4fbd51193251cbb11790bb63aef34aba4
KaioHenrique900 <kaioneziohenrique@gmail.com>
2021-12-15 19:07:55 -0300

    Atualizando get_data.php

--- get_data.php
+++ get_data.php
@@ -1,16 +1,16 @@
 <?php
 
 session_start();
-// array for JSON response
-$response = array();
-
-// connecting to db
-$con_string = "host=ec2-35-168-80-116.compute-1.amazonaws.com port=5432 dbname=d3cnre2oc9uli5 user=blodrftcfvyshh password=0516abc94ad85d3b4e126ff67eae2e73022401049d2862f853034cd2e5e37c61";
-$con = pg_connect($con_string);
-
-$isAuth = false;
 
 function getUserName(){
+	$response = array();
+
+	// connecting to db
+	$con_string = "host=ec2-35-168-80-116.compute-1.amazonaws.com port=5432 dbname=d3cnre2oc9uli5 user=blodrftcfvyshh password=0516abc94ad85d3b4e126ff67eae2e73022401049d2862f853034cd2e5e37c61";
+	$con = pg_connect($con_string);
+
+	$isAuth = false;
+
 	if(isset($_POST['email'])) {
 	    $username = trim($_POST['email']);
 	    $query = pg_query($con, "SELECT email, nome FROM usuario WHERE email='$username'");
