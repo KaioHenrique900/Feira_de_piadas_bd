b7cb086b363d0ebb2f77de938b6d100a54707079
KaioHenrique900 <kaioneziohenrique@gmail.com>
2021-12-29 08:37:05 -0300

    adicionando get_piadas_categoria.php

---
+++ get_piadas_categoria.php
@@ -0,0 +1,56 @@
+<?php
+$response = array();
+
+$con_string = "host=ec2-35-168-80-116.compute-1.amazonaws.com port=5432 dbname=d3cnre2oc9uli5 user=blodrftcfvyshh password=0516abc94ad85d3b4e126ff67eae2e73022401049d2862f853034cd2e5e37c61";
+$con = pg_connect($con_string);
+
+$categoria = $_POST['categoria'];
+$queryCategoria = pg_query($con, "SELECT id_categoria from categoria where descricao='$categoria'");
+
+if (pg_num_rows($queryCategoria)>0){
+    $id_categoria = pg_fetch_array($queryCategoria);
+    $queryPossui = pg_query($con, "SELECT fk_id_piada from possui where fk_id_categoria='id_catgoria'");
+
+    if(pg_num_rows($queryPossui)>0){
+        $queryPossui = pg_fetch_array($queryPossui);
+        $id_piada = $queryPossui['id_piada'];
+
+        $queryPiadas = pg_query($con, "SELECT p.id_piada, p.titulo, p.descricao, p.data_publicacao, u.nome FROM piada as p join usuario as u on p.id_piada='$id_piada' and p.fk_id_usuario = u.id_usuario");
+
+        if (pg_num_rows($queryPiadas)>0){
+            $response["piadas"] = array();
+     
+            while ($row = pg_fetch_array($queryPiadas)) {
+                $piada = array();
+                $piada["id_piada"] = $row["id_piada"];
+                $piada["titulo"] = $row["titulo"];
+                $piada["descricao"] = $row["descricao"];
+                $piada["data_publicacao"] = $row["data_publicacao"];
+                $piada["nome_usuario"] = $row['nome'];
+
+                array_push($response["piadas"], $piada);
+            }
+        }
+
+        else{
+            $response["success"] = 0;
+            $response["error"] = "Algo está errado";
+        }
+
+    }
+
+    else{
+        $response["success"] = 0;
+        $response["error"] = "Algo está errado";
+    }
+}
+
+else{
+    $response["success"] = 0;
+    $response["error"] = "Algo está errado";
+}
+
+pg_close($con);
+echo json_encode($response);
+
+?>
