e0cda2bbe671c3ec2a13a56e0927f9e63c0310d5
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-01-07 07:56:05 -0300

    Adicionando get_user_piadas.php

---
+++ get_user_piadas.php
@@ -0,0 +1,44 @@
+<?php
+$response = array();
+
+$con_string = "host=ec2-35-168-80-116.compute-1.amazonaws.com port=5432 dbname=d3cnre2oc9uli5 user=blodrftcfvyshh password=0516abc94ad85d3b4e126ff67eae2e73022401049d2862f853034cd2e5e37c61";
+$con = pg_connect($con_string);
+
+//$id_usuario = trim($_POST['id_usuario']);
+$email = trim($_POST['email']);
+$queryIdEmail = pg_query($con, "SELECT id_usuario WHERE email='$email'");
+$id_usuario = pg_fetch_array($queryIdEmail);
+
+$queryIdPiada = pg_query($con, "SELECT fk_piada_id_piada FROM publica WHERE fk_usuario_id_usuario = $id_usuario");
+
+if (pg_num_rows($queryIdPiada)>0){
+    $response["piadas"] = array();
+
+    while ($row = pg_fetch_array($queryIdPiada)) {
+        $id_piada = $row['fk_piada_id_piada'];
+
+        $queryPiada = pg_query($con, "SELECT * FROM piada WHERE id_piada = $id_piada");
+        $currentPiada = pg_fetch_array($queryPiada);
+
+        $piada = array();
+        $piada["id_piada"] = $currentPiada["id_piada"];
+        $piada["titulo"] = $currentPiada["titulo"];
+        $piada["descricao"] = $currentPiada["descricao"];
+        $piada["data_publicacao"] = $currentPiada["data_publicacao"];
+        $piada["nome_usuario"] = $currentPiada['nome'];
+
+        array_push($response["piadas"], $piada);
+   }
+	$response["success"] = 1;
+}
+
+else{
+	$response["success"] = 0;
+	$response["error"] = "Usuário não possui piadas";
+}
+
+pg_close($con);
+echo json_encode($response);
+
+
+?>
