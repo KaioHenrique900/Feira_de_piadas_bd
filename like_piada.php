53932bc4d496aba2712faadbcb0f61dd331601bc
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-01-18 09:48:44 -0300

    Atualizando like_piadas

--- like_piada.php
+++ like_piada.php
@@ -41,17 +41,18 @@ if(is_null($username)) {
 		$piadaId =$piadaArray['id_piada'];
 
 		$queryConfere = pg_query($con, "SELECT * from curte where fk_id_usuario='$userId' and fk_id_piada='$piadaId'");
-		if (pg_num_rows($queryConfere)>0){
-			$queryDislike = pg_query($con, "DELETE from curte where fk_id_usuario='$userId' and fk_id_piada='$piadaId'");
-
-			$response["like"] = 0;
-			$response["success"] = 0;
+		if (pg_num_rows($queryConfere)==0){
+			$queryInsert = pg_query($con, "INSERT INTO curte(fk_id_usuario, fk_id_piada) VALUES($userId, $piadaId)");
+	    	$response["like"] = 1;
+	    	$response["success"]=1;
+			
 		}
 	    
 	    else{
-	    	$queryInsert = pg_query($con, "INSERT INTO curte(fk_id_usuario, fk_id_piada) VALUES($userId, $piadaId)");
-	    	$response["like"] = 1;
-	    	$response["success"]=1;
+	    	$queryDislike = pg_query($con, "DELETE from curte where fk_id_usuario='$userId' and fk_id_piada='$piadaId'");
+
+			$response["like"] = 0;
+			$response["success"] = 0;
 	    }
 	}
 
