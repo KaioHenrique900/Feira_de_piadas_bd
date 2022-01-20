2f6df1090750efb989f622edf661a09f3ddb670b
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-01-20 17:03:11 -0300

    Atualizando get_user_piadas.php

--- get_all_piadas.php
+++ get_all_piadas.php
@@ -4,7 +4,21 @@ $response = array();
 $con_string = "host=ec2-35-168-80-116.compute-1.amazonaws.com port=5432 dbname=d3cnre2oc9uli5 user=blodrftcfvyshh password=0516abc94ad85d3b4e126ff67eae2e73022401049d2862f853034cd2e5e37c61";
 $con = pg_connect($con_string);
 
+$piadasCurtidas=Array();
+if (isset($_GET['id_usuario'])){
+    $queryCurtidas = pg_query($con, "SELECT * from curte where fk_id_usuario='$userId'");
+
+    if (pg_num_rows($queryCurtidas)>0){
+        while ($row = pg_fetch_array($queryPiadas)) {
+            $id_piada = $row['fk_id_piada'];
+            $piadasCurtidas.add($id_piada);
+        }
+        
+    }
+}
+
 $queryPiadas = pg_query($con, "SELECT p.id_piada, p.titulo, p.descricao, p.data_publicacao, p.fk_id_usuario, u.nome FROM piada as p join usuario as u on p.fk_id_usuario = u.id_usuario");
+
 
 if (pg_num_rows($queryPiadas)>0){
 	$response["piadas"] = array();
@@ -17,6 +31,13 @@ if (pg_num_rows($queryPiadas)>0){
         $piada["descricao"] = $row["descricao"];
         $piada["data_publicacao"] = $row["data_publicacao"];
 	 	$piada["nome_usuario"] = $row['nome'];
+        $piada["curtida"] = 0;
+        for($i=0;$i<count($piadasCurtidas);$i++){
+            if($piada["id_piada"] == $piadasCurtidas[$i]){
+                $piada["curtida"] = 1;
+                break;
+            }
+        }
 
         array_push($response["piadas"], $piada);
     }
