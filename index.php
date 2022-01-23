e55730eb56072dc9f4926fcdd3b1b46b10c9a3dc
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-01-23 08:36:01 -0300

    Atualizando...

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <h1>Index</h1>
 
-<a href="get_all_categorias.php">get_all_categorias.php</a>
-<a href="get_all_piadas.php">get_all_piadas.php</a>
-<a href="get_data.php">get_data.php</a>
-<a href="get_piadas_categorias.php">get_piadas_categorias.php</a>
-<a href="get_user.php">get_user.php</a>
-<a href="get_user_piadas.php">get_user_piadas.php</a>
-<a href="insert_piada.php">insert_piada.php</a>
-<a href="like_piada.php">like_piada.php</a>
-<a href="login.php">login.php</a>
-<a href="register.php">register.php</a>
-<a href="top_piadas.php">top_piadas.php</a>
+<a href="get_all_categorias.php">get_all_categorias.php</a><br>
+<a href="get_all_piadas.php">get_all_piadas.php</a><br>
+<a href="get_data.php">get_data.php</a><br>
+<a href="get_piadas_categorias.php">get_piadas_categorias.php</a><br>
+<a href="get_user.php">get_user.php</a><br>
+<a href="get_user_piadas.php">get_user_piadas.php</a><br>
+<a href="insert_piada.php">insert_piada.php</a><br>
+<a href="like_piada.php">like_piada.php</a><br>
+<a href="login.php">login.php</a><br>
+<a href="register.php">register.php</a><br>
+<a href="top_piadas.php">top_piadas.php</a><br>
