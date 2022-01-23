243e547299d3ac5acb25cd8dee2ee4b2eb2c59c8
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-01-23 08:37:02 -0300

    Atualizando...

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <a href="get_all_categorias.php">get_all_categorias.php</a><br>
 <a href="get_all_piadas.php">get_all_piadas.php</a><br>
 <a href="get_data.php">get_data.php</a><br>
-<a href="get_piadas_categorias.php">get_piadas_categorias.php</a><br>
+<a href="get_piadas_categoria.php">get_piadas_categorias.php</a><br>
 <a href="get_user.php">get_user.php</a><br>
 <a href="get_user_piadas.php">get_user_piadas.php</a><br>
 <a href="insert_piada.php">insert_piada.php</a><br>
