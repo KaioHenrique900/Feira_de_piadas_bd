205036890ae0a467689df1c7f9c147368b2ea538
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-01-24 10:42:46 -0300

    Atualizando...

--- top_piadas.php
+++ top_piadas.php
@@ -45,7 +45,7 @@ if (pg_num_rows($resultPiadasCurtidasIds)>0){
         $piada["data_publicacao"] = $result["data_publicacao"];
         $piada["id_usuario"] = $result['fk_id_usuario'];
         $piada["nome_usuario"] = $result['nome'];
-        $piada["likes"] = $result["count"];
+        $piada["likes"] = $row["count"];
 
         array_push($response["topPiadas"], $piada);
 	}
