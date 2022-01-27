8a442c1c015d4244021ef4a55f7735e6d12dcc71
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-01-27 08:41:22 -0300

    Atualizando...

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 <a href="get_all_piadas.php">get_all_piadas.php</a><br>
 <a href="get_data.php">get_data.php</a><br>
 <a href="get_piadas_categoria.php">get_piadas_categorias.php</a><br>
-<a href="get_user.php">get_user.php</a><br>
-<a href="get_user_piadas.php">get_user_piadas.php</a><br>
+<a href="https://feira-de-piadas.herokuapp.com/get_user.php">get_user.php</a><br>
+<a href="https://feira-de-piadas.herokuapp.com/get_user_piadas.php">get_user_piadas.php</a><br>
 <a href="insert_piada.php">insert_piada.php</a><br>
 <a href="like_piada.php">like_piada.php</a><br>
 <a href="login.php">login.php</a><br>
